47092eb0e84ab5b306ebc9ced261cf33cf10b2c7
David Weiss <davidweiss@dnavid.com>
2016-07-19 12:25:06 +0000

    Basic layout and link to blog

--- 01-Login/src/views/Main/Container.js
+++ 01-Login/src/views/Main/Container.js
@@ -1,52 +1,43 @@
 import React, { PropTypes as T } from 'react'
 import { Jumbotron, Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 import styles from './styles.module.css'
+
 export class Container extends React.Component {
-        static contextTypes = {
-                router: T.object
-        }
-        render() {
-                let children = null;
-                if (this.props.children) {
-                        children = React.cloneElement(this.props.children, {
-                                auth: this.props.route.auth //sends auth instance to children
-                        })
-                }
-                return (
-                                <div>
-                                <Navbar fixedTop>
-                                <Navbar.Header>
-                                <Navbar.Brand>
-                                <a href="#">DNAvid</a>
-                                </Navbar.Brand>
-                                <Navbar.Toggle />
-                                </Navbar.Header>
-                                <Navbar.Collapse>
-                                <Nav>
-                                <NavItem eventKey={1} href="#">Link</NavItem>
-                                <NavItem eventKey={2} href="#">Link</NavItem>
-                                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                                <MenuItem eventKey={3.1}>Action</MenuItem>
-                                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                                <MenuItem divider />
-                                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                                </NavDropdown>
-                                </Nav>
-                                <Nav pullRight>
-                                <NavItem eventKey={1} href="#">Link Right</NavItem>
-                                <NavItem eventKey={2} href="#">Link Right</NavItem>
-                                </Nav>
-                                </Navbar.Collapse>
-                                </Navbar>
-                                <Jumbotron className={styles.mainJumbotron}>
-                                <h2 className={styles.mainTitle}>
-                                <img src="https://storage.googleapis.com/dnavid/dnavidLogo.png" />
-                                </h2>
-                                {children}
-                </Jumbotron>
-                        </div>
-                        )
-        }
+  static contextTypes = {
+    router: T.object
+  }
+  render() {
+    let children = null;
+    if (this.props.children) {
+    children = React.cloneElement(this.props.children, {
+      auth: this.props.route.auth //sends auth instance to children
+      })
+    }
+
+
+
+
+  return (
+<div>
+    <Navbar fixedTop>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">DNAvid</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+        <Nav pullRight>
+          <NavItem  target="_blank" eventKey={1} href="https://medium.com/@davidweisss/8c49a67330ba">My DNA copyright</NavItem>
+          <NavItem target="_blank" eventKey={2} href="https://medium.com/@davidweisss/f0e47e6c1173" >Roadmap</NavItem>
+        </Nav>
+    </Navbar>
+    <Jumbotron className={styles.mainJumbotron}>
+      <h2 className={styles.mainTitle}>
+        <img src="https://storage.googleapis.com/dnavid/logoDNAvidBlue.png" />
+      </h2>
+      {children}
+    </Jumbotron>
+  </div>
+  )
+  }
 }
 export default Container;
--- 01-Login/webpack.config.js
+++ 01-Login/webpack.config.js
@@ -27,11 +27,11 @@ var config = getConfig({
         html: function (context) {
                 return {
                         'index.html': context.defaultTemplate({
-                                title: 'auth0 React Sample',
+                                title: 'DNAvid - It\'s my genome',
                                 publicPath: isDev ? '' : '',
                                 meta: {
-                                        'name': 'auth0 React Sample',
-                                        'description': 'A minimal reactJS sample application showing auth0 integration'
+                                      'name': 'DNAvid - It\'s my genome',
+                                        'description': 'Global genome governance organization'
                                 }
                         })
                 }
