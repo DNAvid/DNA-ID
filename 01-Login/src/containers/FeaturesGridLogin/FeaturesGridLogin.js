6f144b6f0b008250d5b772ba4efc0463f5e02c31
davidweiss@dnavid.com <davidweiss@dnavid.com>
2017-01-11 13:02:25 +0000

    minor edit to grid login

--- 01-Login/src/containers/FeaturesGridLogin/FeaturesGridLogin.js
+++ 01-Login/src/containers/FeaturesGridLogin/FeaturesGridLogin.js
@@ -1,6 +1,6 @@
 var Radium = require('radium')
 import React from 'react'
-import { Label, Button, Grid, Row, Col } from 'react-bootstrap'
+import { Image, Label, Button, Grid, Row, Col } from 'react-bootstrap'
 
 
 export class FeaturesGridLogin extends React.Component {
@@ -8,6 +8,41 @@ export class FeaturesGridLogin extends React.Component {
     return(
       <Grid>
         <Row>
+          <Col md={4}>
+            <h3> 
+              Very soon! You'll get to try DNA\/ID 
+            </h3> 
+            <p> Invite family to join my DNA network</p>
+            <p> Store my DNA information</p>
+            <p> Control how my DNA is used. Do I:
+              <ul>
+                <li> get paid? </li>
+                <li> participate in research? </li>
+                <li> get updated on research about me?</li>
+                <li> want to limit sharing of my DNA to non-disease info?</li>
+              </ul>
+            </p>
+            <Button target="_blank" bsStyle="default" href="http://bb.dnavid.com/t/define-v1-of-dna-id/">Check the status</Button>
+          </Col>
+          <Col md={4}>
+            <h3> 
+              100% open source 
+            </h3> 
+            <p> It can't be any other way, if it's going to manage our DNA it's gotta be trusted and open</p>
+            <p> Help shape the first genome management system that belongs to all</p>
+            <p> Tackle the hard problems to create a scalable system of access that repects the individual. Unlock our DNA!</p>
+            <Button target="_blank" bsStyle="default" href="https://github.com/DNAvid">Join the open source project</Button>
+          </Col>
+          <Col md={4}>
+            <h3>Support DNAvID</h3>
+            <p>Scan bitcoin address with bitcoin wallet app
+              <Image src="https://storage.googleapis.com/dnavid/dnavidbitcoinaddress.png" style={{maxWidth:'33%'}} responsive />
+              or send to address: <small>15UoJFGFzQcttWEUJUc8H41ZBvvg9JV1Zo</small>
+            </p>
+            <p>Support the project on AngelList</p>
+            <Button target="_blank" bsStyle="default" href="https://angel.co/dnavid">Learn more</Button>&nbsp;
+          </Col>
+          {/*
           <Col md={4}>
             <h2>
               Join the discussion
@@ -21,36 +56,7 @@ export class FeaturesGridLogin extends React.Component {
             <p>
               <Button target="_blank" bsStyle="default" href="http://bb.dnavid.com/">Have your say</Button>&nbsp;
             </p>
-
           </Col>
-          <Col md={4}>
-            <h2> 
-              Claim your DNA ID  
-            </h2> <Label>Coming soon</Label><br></br>
-            <p> Store your genome data</p>
-            <p> Decide what can be used in your genome and for what</p>
-            <p> Safe sharing: anonymise, share only parts, hide parts, of your genome</p>
-            <Button target="_blank" bsStyle="default" href="http://bb.dnavid.com/">Help define the product</Button>
-          </Col>
-          <Col md={4}>
-            <h2> 
-              100% open source 
-            </h2> 
-            <p> Everything is open to contributons, even this page.</p>
-            <p> Help shape the first genome management system that belongs to all</p>
-            <p> Tackle the hard problems to create a scalable system of access that repects the individual. Unlock our DNA!</p>
-            <Button target="_blank" bsStyle="default" href="https://github.com/DNAvid">Join the open source project</Button>
-          </Col>
-          {/*
-            <Col xs={6} md={4}>
-            <Thumbnail src="https://storage.googleapis.com/dnavid/dnavidbitcoinaddress.png" alt="242x200">
-            <h3>Support DNAvID! </h3>
-            <p>Scan Bitcoin Address with Bitcoin Wallet App.</p>
-            <p>
-            <Button target="_blank" bsStyle="default" href="http://bb.dnavid.com/">Learn more</Button>&nbsp;
-            </p>
-            </Thumbnail>
-            </Col>
             */}
           </Row>
           </Grid>
