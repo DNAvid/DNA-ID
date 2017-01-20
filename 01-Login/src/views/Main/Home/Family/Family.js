83985b24fd84cee02044d39c1edcf89da3423d2b
davidweiss@dnavid.com <davidweiss@dnavid.com>
2017-01-20 08:56:43 +0000

    wire invite by email component

--- 01-Login/src/views/Main/Home/Family/Family.js
+++ 01-Login/src/views/Main/Home/Family/Family.js
@@ -1,15 +1,6 @@
+import axios from 'axios';
 import React from 'react';
 import {ProgressBar, FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap'
-
-function FieldGroup({ id, label, help, ...props }) {
-  return (
-    <FormGroup controlId={id}>
-      <ControlLabel>{label}</ControlLabel>
-      <FormControl {...props} />
-      {help && <HelpBlock>{help}</HelpBlock>}
-    </FormGroup>
-  );
-}
 
 
 class PercentFamily extends React.Component {
@@ -25,18 +16,93 @@ class PercentFamily extends React.Component {
   }
 };
 
+function FieldGroup({ id, label, help, ...props }) {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
+
+
 class InviteFamily extends React.Component {
-  render()
-  {
+  constructor(props){
+    super(props);
+    this.state = {
+      name:'',
+      relationship:'',
+      content:'',
+      toemail:''
+    }
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeToemail = this.handleChangeToemail.bind(this);
+    this.handleChangeRelationship = this.handleChangeRelationship.bind(this);
+    this.handleChangeContent = this.handleChangeContent.bind(this);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChangeName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleChangeToemail(event) {
+    this.setState({
+      toemail: event.target.value
+    });
+  }
+
+  handleChangeRelationship(event) {
+    this.setState({
+      relationship: event.target.value
+    });
+  }
+
+  handleChangeContent(event) {
+    this.setState({
+      content: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    let profile = JSON.parse(localStorage.getItem('profile'))
+    axios.post('https://wt-davidweiss-dnavid_com-0.run.webtask.io/dnavid-sendgrid', {
+      from_email: profile.email || 'davidweiss@dnavid.com',
+      to_email: this.state.toemail,
+      subject: profile.name + ' invites you to join their family on DNA\\/ID as their ' + this.state.relationship,
+      content: 'Hi '+ this.state.name + ', claim your invite on https://dnavid.com \n\n' 
+      + profile.name + ' sent this message: \n\n' 
+      + this.state.content
+    })
+      .then(function (response) {
+        console.log("Email sent, response was : ", response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    event.preventDefault();
+  }
+
+  render(){
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <FieldGroup
+          value={this.state.name}
+          onChange={this.handleChangeName}
           id="formControlsText"
           type="text"
           label="Name"
           placeholder="Name of person to invite"
         />
         <FieldGroup
+          value={this.state.toemail}
+          onChange={this.handleChangeToemail}
           id="formControlsEmail"
           type="email"
           label="Email address"
@@ -44,14 +110,19 @@ class InviteFamily extends React.Component {
         />
         <FormGroup controlId="formControlsSelect">
           <ControlLabel>Relationship</ControlLabel>
-          <FormControl componentClass="select" placeholder="select">
+          <FormControl
+            componentClass="select"
+            value={this.state.relationship}
+            onChange={this.handleChangeRelationship}
+            placeholder="select"
+          >
             <option value="select">Select</option>
             <option value="sister">Sister</option>
             <option value="brother">Brother</option>
             <option value="mother">Mother</option>
             <option value="father">Father</option>
             <option value="spouse">Spouse</option>
-            <option value="son">son</option>
+            <option value="son">Son</option>
             <option value="daughter">Daughter</option>
             <option value="cousin">Cousin</option>
             <option value="friend">Friend</option>
@@ -59,9 +130,13 @@ class InviteFamily extends React.Component {
         </FormGroup>
         <FormGroup controlId="formControlsTextarea">
           <ControlLabel>Message</ControlLabel>
-          <FormControl componentClass="textarea" placeholder="Hi, I'd like to invite you to join DNA\/ID. DNA information is much more valuable when shared!" />
+          <FormControl 
+            value={this.state.content}
+            onChange={this.handleChangeContent}
+            componentClass="textarea"
+            placeholder="Hi, I'd like to invite you to join DNA\/ID. DNA information is much more valuable when shared!" />
         </FormGroup>
-        <Button type="submit">
+        <Button type="submit" value="Submit">
           Invite to join DNA\/ID
         </Button>
       </form>
@@ -83,4 +158,3 @@ export class Family  extends React.Component {
 }
 
 export default Family;
-
