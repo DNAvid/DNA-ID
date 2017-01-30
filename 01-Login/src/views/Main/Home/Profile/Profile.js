96b38f0db77dc18bb0ac90b8bf83de465f1aa93b
davidweiss@dnavid.com <davidweiss@dnavid.com>
2017-01-30 10:52:42 +0000

    questionnaire stateful + server conn

--- 01-Login/src/views/Main/Home/Profile/Profile.js
+++ 01-Login/src/views/Main/Home/Profile/Profile.js
@@ -1,5 +1,13 @@
+import axios from 'axios'
 import React, { PropTypes as T } from 'react';
-import {Button, FormGroup, ControlLabel, FormControl, HelpBlock, Row, Col, Image} from 'react-bootstrap';
+import {Button, FormGroup, ControlLabel, FormControl, HelpBlock, Row, Col, Grid, Image} from 'react-bootstrap';
+
+function getUserProfile(){
+return(  axios({
+    baseURL:'https://wt-davidweiss-dnavid_com-0.run.webtask.io',
+    url: 'doesUserHaveProfile.js',
+    method: 'get',
+    headers: {Authorization: 'Bearer ' + localStorage.id_token}}))}
 
 class FieldGroup extends React.Component{
   render(){
@@ -14,58 +22,367 @@ class FieldGroup extends React.Component{
 }
 
 class ChoosePseudo extends React.Component {
-  render(){
+  constructor(props){
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
+  handleSubmit(event){
+    this.props.onSubmit(event.target.value)
+
+  }
+  handleChange(event){
+    this.props.onChange(event.target.value)
+
+  }
+  render(){
     return(
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <FieldGroup
           id="formControlsText"
-          type="Text"
-          label="Choose your DNA ID"
+          value={this.props.value}
+          onChange={this.handleChange}
+          type="text"
+          label="Choose your user name"
           placeholder="1-15 characters, letters and numbers only"
         />
-
-      <FormGroup controlId="formControlsTextarea">
-        <ControlLabel>Bio</ControlLabel>
-        <FormControl style={{minHeight: '80px'}} componentClass="textarea" placeholder="Why I'm interested in DNA. Is there something that runs in my family that could be due to DNA. Max. 120 characters." />
-      </FormGroup>
-
-      <Button onClick={ () => {alert("Sorry, cannot accept more DNA IDs for now. You are on the waiting list. Please be patient.")}}>
-        Alright!
-      </Button>
-    </form>
+        <Button type="submit" value="Submit">
+          Get it
+        </Button>
+      </form>
     )
   }
 }
 
-export class Profile  extends React.Component {
+class WriteBio extends React.Component{
+  constructor(props){
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event){
+    this.props.onSubmit(event.target.value)
+
+  }
+
+  handleChange(event){
+    this.props.onChange(event.target.value)
+
+  }
+
   render(){
-    let profile = this.props.auth.getProfile() 
-    return (
-      <div>
-        <Row style={{marginBottom:'50px'}}> 
-          <Col sm={12} md={6}>
-            <ChoosePseudo/>
-          </Col> 
-        </Row>
-        <Row >
-          <Col sm={12} md={6} >
-            <Image responsive style={{maxHeight:'60px'}} src={profile.picture}/>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={12} md={6}>
-            <h3>My profile</h3>
-            <p><strong>Name: </strong> {profile.name}</p>
-            <p><strong>Email: </strong> {profile.email}</p>
-            <p><strong>Nickname: </strong> {profile.nickname}</p>
-            <p><strong>Created At: </strong> {profile.created_at}</p>
-            <p><strong>Updated At: </strong> {profile.updated_at}</p>
-          </Col>
-        </Row>
-      </div>
+    return(
+      <Row className="show-grid">
+        <Col xs={12}>
+
+          <form onSubmit={this.handleSubmit}>
+            <FieldGroup
+              id="formControlsTextarea"
+              value={this.props.value}
+              onChange={this.handleChange}
+              componentClass="textarea"
+              label="Bio"
+              placeholder={this.props.bio ? this.props.bio : "Why I'm interested in DNA. Is there something that runs in my family that could be due to DNA. Max. 120 characters."}
+              style={{minHeight: '80px'}}
+            />
+            <Button type="submit" value="Submit">
+              Update bio 
+            </Button>
+          </form>
+
+        </Col>
+      </Row>
+
     )
   }
 }
 
+class SetPicture extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event){
+    this.props.onSubmit(event.target.value)
+  }
+
+  handleChange(event){
+    this.props.onChange(event.target.value)
+
+  }
+
+  render(){
+    return(
+      <div>
+      <Row className="show-grid">
+        <Col xs={3}>
+          <Image src={this.props.picture|| "https://storage.googleapis.com/dnavid/egg.png"} thumbnail/>
+        </Col>
+        <Col xs={9}><h1>
+            {this.props._id}
+          </h1>
+        </Col>
+      </Row>
+      <Row>
+          <FieldGroup
+            id="formControlsFile"
+            type="file"
+            label="Upload picture"
+          />
+      </Row>
+  </div>
+    )
+  }
+}
+
+class SetProfileDetails extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleChangeFirstName = this.handleChangeFirstName.bind(this)
+    this.handleChangeLastName = this.handleChangeLastName.bind(this)
+    this.handleChangeEmail = this.handleChangeEmail.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(event){
+    this.props.onSubmit(event.target.value)
+
+  }
+  handleChangeFirstName(event){
+    this.props.onChangeFirstName(event.target.value)
+  }
+  handleChangeLastName(event){
+    this.props.onChangeLastName(event.target.value)
+  }
+  handleChangeEmail(event){
+    this.props.onChangeEmail(event.target.value)
+  }
+  render(){
+    const {firstName, lastName, email} = this.props
+    return(
+      <Row>
+        <Col sm={12} md={6}>
+          <h4>Personal Details</h4>
+          <form onSubmit={this.handleSubmit}>
+            <FieldGroup
+              id="formControlsText"
+              type="text"
+              label="First Name"
+              placeholder={firstName?firstName:"First name"}
+              value={this.props.valueFirstName}
+              onChange={this.handleChangeFirstName}
+            />
+            <FieldGroup
+              id="formControlsText"
+              type="text"
+              label="Last Name"
+              placeholder={lastName?lastName:"Last name"}
+              value={this.props.valueLastName}
+              onChange={this.handleChangeLastName}
+            />
+            <FieldGroup
+              id="formControlsEmail"
+              type="email"
+              label="Email address"
+              placeholder={email?email:"Email"}
+              value={this.props.valueEmail}
+              onChange={this.handleChangeEmail}
+            />
+
+          <Button type="submit" value="Submit">
+            Update details
+          </Button>
+        </form>
+      </Col>
+    </Row>
+    )
+  }
+}
+
+
+  //////////
+ // Main //
+//////////
+export class Profile  extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      userHasProfile:'loading',
+      pseudo: undefined,
+      bio: undefined,
+      picture: undefined,
+      firstName: undefined,
+      LastName: undefined
+    }
+
+    this.handleChangeFirstName= this.handleChangeFirstName.bind(this);
+    this.handleChangeLastName= this.handleChangeLastName.bind(this);
+    this.handleChangeEmail= this.handleChangeEmail.bind(this);
+    this.handleChangePicture= this.handleChangePicture.bind(this);
+    this.handleChangeBio= this.handleChangeBio.bind(this);
+    this.handleChangePseudo= this.handleChangePseudo.bind(this);
+
+    this.handleSubmitDetails= this.handleSubmitDetails.bind(this);
+    this.handleSubmitPicture= this.handleSubmitPicture.bind(this);
+    this.handleSubmitBio= this.handleSubmitBio.bind(this);
+    this.handleSubmitPseudo = this.handleSubmitPseudo.bind(this);
+  }
+
+  handleChangeFirstName(value) {
+    this.setState({
+      firstName: value
+    });
+  }
+  handleChangeLastName(value) {
+    this.setState({
+      lastName: value
+    });
+  }
+  handleChangeEmail(value) {
+    this.setState({
+      email: value
+    });
+  }
+  handleChangePicture(value) {
+    this.setState({
+      picture: value
+    });
+  }
+  handleChangePseudo(value) {
+    this.setState({
+      pseudo: value
+    });
+  }
+  handleChangeBio(value) {
+    this.setState({
+      bio: value
+    });
+  }
+
+
+  handleSubmitDetails(value) {
+   axios({
+      baseURL:'https://wt-davidweiss-dnavid_com-0.run.webtask.io',
+      url: 'updateUser.js',
+      params: {
+        category: "profile",
+        key: "firstName",
+        text: this.state.firstName
+      },
+      method: 'post',
+      headers: {Authorization: 'Bearer ' + localStorage.id_token}}) 
+
+    axios({
+      baseURL:'https://wt-davidweiss-dnavid_com-0.run.webtask.io',
+      url: 'updateUser.js',
+      params: {
+        category: "profile",
+        key: "lastName",
+        text: this.state.lastName
+      },
+      method: 'post',
+      headers: {Authorization: 'Bearer ' + localStorage.id_token}}) 
+
+    axios({
+      baseURL:'https://wt-davidweiss-dnavid_com-0.run.webtask.io',
+      url: 'updateUser.js',
+      params: {
+        category: "profile",
+        key: "email",
+        text: this.state.email
+      },
+      method: 'post',
+      headers: {Authorization: 'Bearer ' + localStorage.id_token}}) 
+    console.log(this.state.firstName, this.state.lastName, this.state.email)
+  }
+  handleSubmitPicture(value) {
+  }
+  handleSubmitPseudo(value) {
+    axios({
+      baseURL:'https://wt-davidweiss-dnavid_com-0.run.webtask.io',
+      url: 'createUser.js',
+      params: {
+        pseudo: this.state.pseudo
+      },
+      method: 'post',
+      headers: {Authorization: 'Bearer ' + localStorage.id_token}}) 
+        .then( 
+          res => {
+            getUserProfile().then( 
+              res => {
+                this.setState({userHasProfile:res.data})
+              }
+            )
+
+          }
+        )
+  }
+  handleSubmitBio(value) {
+    axios({
+      baseURL:'https://wt-davidweiss-dnavid_com-0.run.webtask.io',
+      url: 'updateUser.js',
+      params: {
+        category: "profile",
+        key: "bio",
+        text: this.state.bio
+      },
+      method: 'post',
+      headers: {Authorization: 'Bearer ' + localStorage.id_token}}) 
+  }
+
+  componentWillMount(){ 
+    getUserProfile().then( 
+      res => {
+        this.setState({userHasProfile:res.data})
+      }
+    )
+  }
+  render(){
+    if(this.state.userHasProfile=='loading'){ return <div> Looking up your info...</div>}
+
+    if(!this.state.userHasProfile){ 
+      return (
+        <ChoosePseudo value={this.state.pseudo} onChange={this.handleChangePseudo} onSubmit={this.handleSubmitPseudo} />
+      )
+    }
+
+    else{ 
+      let user = this.state.userHasProfile 
+      let auth0User= this.props.auth.getProfile()
+      const _id = user._id
+      const {bio, picture, firstName, lastName, email}=user.profile
+
+      return (
+        <Grid>
+          <SetPicture 
+            value={this.state.picture} 
+            onChange={this.handleChangePicture} 
+            onSubmit={this.handleSubmitPicture} 
+            picture={picture} 
+            _id={_id} />
+          <WriteBio 
+            value={this.state.bio} 
+            onChange={this.handleChangeBio} 
+            onSubmit={this.handleSubmitBio} 
+            bio={bio} />
+          <SetProfileDetails 
+            onSubmit={this.handleSubmitDetails}
+            onChangeFirstName={this.handleChangeFirstName}
+            onChangeLastName={this.handleChangeLastName}
+            onChangeEmail={this.handleChangeEmail}
+            valueFirstName={this.state.firstName}
+            valueLastName={this.state.lastName}
+            valueEmail={this.state.email}
+            firstName={firstName} 
+            lastName={lastName} 
+            email={email}
+          />
+        </Grid>
+      )
+    }
+  }
+}
 export default Profile;
--- 01-Login/src/views/Main/Home/Share/Share.js
+++ 01-Login/src/views/Main/Home/Share/Share.js
@@ -1,61 +1,146 @@
+import axios from 'axios'
+import { hashHistory } from 'react-router';
 import React from 'react';
-import { Checkbox, FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
+import {CheckBox, Button, FormGroup, ControlLabel, FormControl, HelpBlock, Row, Col, Grid, Image} from 'react-bootstrap';
 
-export class Share extends React.Component {
+
+
+function getUserProfile(){
+  return(
+    axios(
+      {
+        baseURL:'https://wt-davidweiss-dnavid_com-0.run.webtask.io',
+        url: 'doesUserHaveProfile.js',
+        method: 'get',
+        headers: {Authorization: 'Bearer ' + localStorage.id_token}
+      }
+    )
+  )
+}
+
+class StatefulCheckbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      [this.props.question]: this.props.answer
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.checked;
+    var name = target.name;
+    console.log(name,value)
+    this.setState({
+      [name]: value
+    });
+
+    axios(
+      {
+        baseURL:'https://wt-davidweiss-dnavid_com-0.run.webtask.io',
+        url: 'updateUser.js',
+        params: {
+          category: "share",
+          subcategory: this.props.category,
+          key: name,
+          text: value},
+        method: 'post',
+        headers: {Authorization: 'Bearer ' + localStorage.id_token}
+      }
+    )
+  }
+
   render() {
+    console.log("db answer is:",typeof this.props.answer)
     return (
-        <form>
-          <FormGroup>
-            <ControlLabel>Conditions for using my DNA information.</ControlLabel>
-          </FormGroup>
-          <FormGroup>
-            <FormControl.Static>
-              Financial compensation for use of my DNA information
-            </FormControl.Static> 
-            <Checkbox>
-              From for profit research
-            </Checkbox>
-            <Checkbox>
-              From academic research
-            </Checkbox>
-            <Checkbox>
-              From use in the clinic
-            </Checkbox>
-            <FormControl.Static>
-              Return of results from analysis of my DNA information
-            </FormControl.Static> 
-            <Checkbox>
-              I want to be informed of results obtained about me that impact my health
-            </Checkbox>
-            <Checkbox>
-              I do not want to be informed about results that impact my health but for which there is no preventive or corrective remedy known
-            </Checkbox>
+      <div>
+      <label>
+        <input
+          name= {this.props.question}
+          type="checkbox"
+          defaultChecked={this.props.answer==='true'}
+          checked={this.state.name}
+          onChange={this.handleInputChange} />
+        {" "+this.props.question}
+      </label>
+      <br/>
+  </div>
 
-            <FormControl.Static>
-              Participation in research
-            </FormControl.Static>
-            <Checkbox>
-              I am available for questions from researchers who are analysing my DNA information      
-            </Checkbox>    
-            <Checkbox>
-              I wish to be asked for permission every time my DNA is included in a study (uncheck if automatic enrollment is ok)      
-            </Checkbox>
-
-            <FormControl.Static>
-              Privacy
-            </FormControl.Static>
-            <Checkbox>
-              I do not wish to share personal information other than DNA information and I don't allow attemps at reidentification
-            </Checkbox>    
-            <Checkbox>
-              I do not wish to share DNA information that, with current state of knowledge, could lead to disease risk identification
-            </Checkbox>    
-          </FormGroup>
-        </form>
     );
   }
 }
 
-export default Share;
+class QuestionGroup extends React.Component{
+  render(){
+    const questions = this.props.questions
+    return(
+      <div>
+        <h4>{this.props.category} </h4>
+        <br/>
+        {
+          Object.keys(questions).map((question)=>
+            <StatefulCheckbox 
+              question={question} 
+              answer={questions[question]}
+              category={this.props.category}/>
 
+            )
+        }
+        <br/>
+      </div>  
+    )
+  }
+}
 
+class Questionnaire extends React.Component{
+  render(){
+    var share = this.props.share
+    var categories = Object.keys(share)
+    return(  
+      <form>
+        {
+          categories.map((category) =>
+            <QuestionGroup 
+              category={category}
+              questions={share[category]}
+            />
+            )
+        }
+
+      </form>
+    )
+  }
+}
+
+class Share extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      user:"loading",
+    }
+  }
+
+  componentWillMount(){ 
+    getUserProfile().then( 
+      res => {
+        this.setState({user:res.data})
+      }
+    )
+  }
+
+  render(){
+    if(this.state.user==='loading'){ 
+      return(<div>Loading your sharing preferences</div>)}
+    else if(!this.state.user){
+      return( <div>Redirect to Home</div>)
+    }else{
+      return(
+        <Questionnaire
+          share={this.state.user.share}
+        />
+      )} 
+  }
+}
+
+export default Share 
