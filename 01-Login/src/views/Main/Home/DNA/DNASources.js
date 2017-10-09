b1c0ff6b34866c7727ee2a3830e222df479e4885
davidweiss@dnavid.com <davidweiss@dnavid.com>
2017-10-09 13:30:22 +0000

    DNA upload working w/ ipfs

--- 01-Login/src/views/Main/Home/DNA/DNASources.js
+++ 01-Login/src/views/Main/Home/DNA/DNASources.js
@@ -37,54 +37,79 @@ export class DNASource extends React.Component {
   constructor(props){
     super(props)
     this.state={
-      "file": this.props.source.file,
-      "helpMessage":"Select file (above) and upload (below)"}
+      "fileHash": this.props.source.file.hash ,
+      "fileSecret": this.props.source.file.secret,
+      "filetoupload": "",
+      "helpMessage":"Select file above to upload"}
 
-    this.handleChange = this.handleChange.bind(this)  
-    this.handleSubmit = this.handleSubmit.bind(this)  
+    this.handleSubmitDownload = this.handleSubmitDownload.bind(this)  
+    this.handleChangeUpload = this.handleChangeUpload.bind(this)  
+    this.handleSubmitUpload = this.handleSubmitUpload.bind(this)  
   }
 
-  handleSubmit(event) {
+  handleSubmitUpload(event) {
     var data = new FormData();
-    data.append('file', this.state.file);
-    var config = {
-      onUploadProgress: function(progressEvent) {
-        var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
-      },
-      params:{
-        pseudo:this.props.pseudo,
-        key:this.props.sourceName
-      },
-      headers: {
-        Authorization: "Bearer " + localStorage.id_token 
-      }
-    };
+    // Approx 40-digit random number secret
+    const secret = crypto.getRandomValues(new Uint32Array(4)).join('')
+    data.append("interactive", 0);
+    data.append("secret", secret);
+    data.append('filetoupload', this.state.filetoupload, 'filetoupload');
     this.setState({"helpMessage":"Uploading..."})
-    axios.put('https://wt-davidweiss-dnavid_com-0.run.webtask.io/transferFiles.js', data, config)
+    // upload to ipfs
+    console.log("Uploading to IPFS.")
+    axios.post('https://dnacoinstorage.com/fileUpload', data)
       .then(res => {
+        console.log(res)
         this.setState({ "helpMessage":"Success: file uploaded"})
+        // Add secret and hash to user profile.
+        setUser({
+          category: "DNA",
+          subcategory: this.props.sourceName,
+          key: "file.hash",
+          text: res.data.hash
+        })
+        setUser({
+          category: "DNA",
+          subcategory: this.props.sourceName,
+          key: "file.secret",
+          text:  secret
+        })
+       this.setState({"fileSecret": secret}) 
+       this.setState({"fileHash": res.data.hash}) 
+
       })
       .catch(err => {
         console.log("err is:", err)
-        this.setState({ "helpMessage":"Aw, snap! Some's wrong.. Make sure the file transfer takes less than 35 seconds and all files are less than 16MB combined."})
+        this.setState({ "helpMessage":"Aw, snap! Something's wrong.."})
       });
-    console.log(this.props.sourceName)
-    setUser({
-     category: "DNA",
-     subcategory: this.props.sourceName,
-      key: "file",
-      text: this.state.file.name
-    })
   }
 
-  handleChange (event){
-    this.setState({"file" : event.target.files[0],
+  handleChangeUpload (event){
+    console.log('event.target.files[0]',event.target.files[0])
+    this.setState({"filetoupload" : event.target.files[0],
       "helpMessage": "Click button below to upload selected file."})
+  }
+
+
+  handleSubmitDownload(event) {
+    var data = new FormData();
+    data.append("secret", this.state.fileSecret);
+    data.append("fileMultihash", this.state.fileHash);
+    // Download from ipfs
+    console.log("Dowloading from IPFS.")
+    axios.post('https://dnacoinstorage.com/fileDownload', data)
+      .then(res => {
+        console.log(res.data)
+        })
+      .catch(err => {
+        console.log("err is:", err)
+        alert('Download failed.')
+      });
   }
 
   render(){
     var {badge, heading, description}= this.props.source
-    
+    //var ipfsUrl = 'https/ipfs.io/ipfs/'.concat(file.hash)
     return (
       <div style={{marginTop:'40px', marginBottom:'60px'}}>
         <Media>
@@ -94,13 +119,16 @@ export class DNASource extends React.Component {
           <Media.Body>
             <Media.Heading>{heading}</Media.Heading> 
             <p>{description}</p>
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmitDownload}>
+              {this.state.fileHash.length ? <div style={{color:'green',marginBottom:'10px'}}>File available <Button type='submit' bsStyle='success' bsStyle='xsmall'>here</Button></div>: ''}
+            </form>
+            <form onSubmit={this.handleSubmitUpload}>
               <FieldGroup
                 id="formControlsFile"
                 type="file"
-                label="Upload file to private space"
+                label="Upload file"
                 help= {this.state.helpMessage}
-                onChange={this.handleChange}
+                onChange={this.handleChangeUpload}
               />
               <Button type="submit">
                 Upload
